2964e9103fe5ba98a68944ba46d865d865edd6b5
anaantunes26 <152395214+anaantunes26@users.noreply.github.com>
2023-12-04 20:34:09 +0100

    Changes

--- Golffirstpart.py
+++ Golffirstpart.py
@@ -1,53 +1,84 @@
 import streamlit as st
 
-st ("class Hole:
-  def __init__(self, hole_number, par, stroke_index):
-      self.hole_number = hole_number
-      self.par = par
-      self.stroke_index = stroke_index
+class Hole:
+    def __init__(self, hole_number, par, stroke_index):
+        self.hole_number = hole_number
+        self.par = par
+        self.stroke_index = stroke_index
 
-  def __str__(self):
-      return f"Hole {self.hole_number} - Par: {self.par}, Stroke Index: {self.stroke_index}"
+    def __str__(self):
+        return f"Hole {self.hole_number} - Par: {self.par}, Stroke Index: {self.stroke_index}"
 
 class Player:
-  def __init__(self, name, handicap):
-      self.name = name
-      self.handicap = handicap
-      self.scores = {}
+    def __init__(self, name, handicap):
+        self.name = name
+        self.handicap = handicap
+        self.scores = {}
 
-  def play_hole(self, hole, strokes):
-      self.scores[hole.hole_number] = strokes
+    def play_hole(self, hole, strokes):
+        self.scores[hole.hole_number] = strokes
 
-  def calculate_total_score(self):
-      return sum(self.scores.values())
+    def calculate_total_score(self):
+        return sum(self.scores.values())
 
-  def __str__(self):
-      return f"{self.name} - Handicap: {self.handicap}, Total Score: {self.calculate_total_score()}"
+    def __str__(self):
+        return f"{self.name} - Handicap: {self.handicap}, Total Score: {self.calculate_total_score()}"
 
 class Course:
-  def __init__(self, name, holes):
-      self.name = name
-      self.holes = holes
+    def __init__(self, name, holes):
+        self.name = name
+        self.holes = holes
 
 def get_player_info():
-  name = input("Enter player's name: ")
-  handicap = int(input("Enter your handicap: "))
-  return Player(name, handicap)
+    name = st.text_input("Enter player's name:")
+    handicap = st.number_input("Enter your handicap:", min_value=0)
+    return Player(name, handicap)
 
 def create_custom_course():
-  course_name = input("Enter the name of the course: ")
-  holes = []
-  for i in range(1, 19):
-      print(f"Enter details for Hole {i}:")
-      par = int(input("Par: "))
-      stroke_index = int(input("Stroke Index: "))
-      holes.append(Hole(i, par, stroke_index))
-  return Course(course_name, holes)
+    course_name = st.text_input("Enter the name of the course:")
+    holes = []
+    for i in range(1, 19):
+        st.write(f"Enter details for Hole {i}:")
+        par = st.number_input(f"Par for Hole {i}:", min_value=1)
+        stroke_index = st.number_input(f"Stroke Index for Hole {i}:", min_value=1)
+        holes.append(Hole(i, par, stroke_index))
+    return Course(course_name, holes)
 
 def get_course_choice():
-  print("Choose a golf course:")
-  print("1. Ostschweizerischer Golfclub Niederbüren")
-  print("2. Golfclub Appenzell")
-  print("3. Golfpark Waldkirch")
-  print("4. Create a new course")
-  choice = input("Enter your choice (1-4): ")")
+    st.write("Choose a golf course:")
+    st.write("1. Ostschweizerischer Golfclub Niederbüren")
+    st.write("2. Golfclub Appenzell")
+    st.write("3. Golfpark Waldkirch")
+    st.write("4. Create a new course")
+    choice = st.selectbox("Enter your choice (1-4):", ('1', '2', '3', '4'))
+    
+    if choice == '1':
+        predefined_holes = [Hole(1, 5, 13), Hole(2, 4, 9), ...]  # Define holes here
+        return Course("Ostschweizerischer Golfclub Niederbüren", predefined_holes)
+    # Rest of the code remains the same for other choices
+
+def main():
+    st.title("Golf Score Tracker")
+    course = get_course_choice()
+    players = []
+
+    while True:
+        player = get_player_info()
+        players.append(player)
+
+        for hole in course.holes:
+            st.write(hole)
+            strokes = st.number_input(f"Enter strokes for Hole {hole.hole_number}:")
+            player.play_hole(hole, strokes)
+
+        another_player = st.radio("Is there another player?", ('Yes', 'No'))
+        if another_player == 'No':
+            break
+
+    players.sort(key=lambda x: x.calculate_total_score())
+    st.header(f"Results at {course.name}:")
+    for player in players:
+        st.write(player)
+
+if __name__ == "__main__":
+    main()
