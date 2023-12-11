6320ba7b67cac7e020e83aa12930ddf0212dbf93
anaantunes26 <152395214+anaantunes26@users.noreply.github.com>
2023-12-11 22:33:05 +0100

    P

--- NICK.py
+++ NICK.py
@@ -174,5 +174,97 @@ def main():
 if __name__ == "__main__":
     main()
 
+import streamlit as st
+
+class Hole:
+    def __init__(self, hole_number, par, stroke_index):
+        self.hole_number = hole_number
+        self.par = par
+        self.stroke_index = stroke_index
+
+    def calculate_points(self, handicap, strokes):
+        # Implement Stableford point calculation here
+        # Use the provided formula
+        return max(0, 2 + (self.par - strokes))
+
+class Course:
+    def __init__(self, name, holes):
+        self.name = name
+        self.holes = holes
+
+class Player:
+    def __init__(self, name, handicap):
+        self.name = name
+        self.handicap = handicap
+
+class GolfRound:
+    def __init__(self, course, player):
+        self.course = course
+        self.player = player
+        self.strokes_by_hole = {}
+        self.points_by_hole = {}
+
+    def play_hole(self, hole, strokes):
+        # TODO: Implement solid validations for strokes - 1 max. 10?
+        self.strokes_by_hole[hole.hole_number] = strokes
+        self.calculate_points_by_hole(hole, strokes)
+
+    def calculate_points_by_hole(self, hole, strokes):
+        self.points_by_hole[hole.hole_number] = hole.calculate_points(self.player.handicap, strokes)
+
+    def calculate_total_strokes(self):
+        return sum(self.strokes_by_hole.values())
+
+    def calculate_total_points(self):
+        return sum(self.points_by_hole.values())
+
+    def __str__(self):
+        return f"{self.player.name} - Handicap: {self.player.handicap}, Total Strokes: {self.calculate_total_strokes()}, Total Points: {self.calculate_total_points()}"
+
+def create_golf_round(course):
+    name = st.text_input("Enter player's name:")
+    handicap = st.number_input("Enter your handicap:", min_value=0, max_value=36)
+    player = Player(name, handicap)
+    return GolfRound(course, player)
+
+def create_custom_course():
+    course_name = st.text_input("Enter the name of the course:")
+    holes = []
+    for i in range(1, 19):
+        st.write(f"Enter details for Hole {i}:")
+        par = st.number_input(f"Hole {i} - Par:", min_value=3, max_value=5)
+        stroke_index = st.number_input(f"Hole {i} - Stroke Index:", min_value=1, max_value=18)
+        holes.append(Hole(i, par, stroke_index))
+    return Course(course_name, holes)
+
+def main():
+    st.title("Golf Round Tracker")
+
+    course = get_course_choice()
+
+    rounds = []
+    while True:
+        golf_round = create_golf_round(course)
+        rounds.append(golf_round)
+
+        for hole in course.holes:
+            st.write(hole)
+
+            # Ask the player to enter the strokes for the hole and save the result
+            strokes = st.number_input(f"Enter strokes for Hole {hole.hole_number}:", min_value=1, max_value=10)
+            golf_round.play_hole(hole, strokes)
+
+        another_player = st.radio("Is there another player?", ("Yes", "No"))
+        if another_player == 'No':
+            break
+
+    rounds.sort(key=lambda x: x.calculate_total_points(), reverse=True)
+
+    st.write(f"\nResults at {course.name}:")
+    for round in rounds:
+        st.write(round)
+
+if __name__ == "__main__":
+    main()
 
 
