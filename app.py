e4417f89e02fb9c64dfaa41cacef6196f34f58c7
anaantunes26 <152395214+anaantunes26@users.noreply.github.com>
2023-12-11 17:10:04 +0100

    Push

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.write("Horray, we connected everything")
 st.write("Thank you for being here")
+if st.button ("Roll the dice!")
+    st.header("Results")
+    results = roll_the_dice(6000)
+    st.write(results)
