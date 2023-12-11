b9a30bea5b29cbf2cdb6a275ecad08d2beeb8b0d
anaantunes26 <152395214+anaantunes26@users.noreply.github.com>
2023-12-12 00:47:23 +0100

    X

--- FABIO.py
+++ FABIO.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+
+
 st.title("Welcome to the Golf App!")
 
 menu_choice = st.sidebar.radio("Menu", ["Home", "Register", "Login", "Exit"])
@@ -42,44 +44,3 @@ elif menu_choice == "Exit":
     st.balloons()
     st.write("Goodbye!")
     st.stop()
-
-class GolfApp:
-    def __init__(self):
-        self.accounts = []
-        self.courses = []
-
-    def load_accounts(self):
-        # Muster-Accounts
-        self.accounts = [
-            {"Benutzername": "casual_golfer123", "E-Mail": "casualgolfer123@example.com", "Passwort": "Golfer123!", "Mitgliedschaft": "Basic"},
-            {"Benutzername": "golf_enthusiast45", "E-Mail": "golfenthusiast45@example.com", "Passwort": "GreenSwing45@", "Mitgliedschaft": "Premium"},
-            {"Benutzername": "pro_golfer89", "E-Mail": "progolfer89@example.com", "Passwort": "ProPassion89$", "Mitgliedschaft": "Elite"}
-        ]
-        st.write("Accounts wurden geladen!")
-
-    def load_courses(self):
-        # Golfplätze in der Schweiz
-        self.courses = [
-            {"Swiss Alps Golf Club", "Crans-Montana", "Beschreibung:" "Ein atemberaubender Golfplatz in den Schweizer Alpen mit Panoramablicken und anspruchsvollen Löchern."},
-            {"Lake Geneva Golf Resort", "Genf", "Beschreibung:" "Ein exklusiver Golfplatz am Genfer See mit Blick auf das Wasser und eine perfekte Kombination aus Herausforderung und Schönheit."},
-            {"Zurich Valley Golf Club", "Zürich", "Beschreibung:" "Ein moderner Golfplatz in der Nähe von Zürich, der eine erstklassige Spielumgebung mit hochwertigen Einrichtungen bietet."}
-        ]
-        st.write("Golfplätze wurden geladen!")
-
-# Erstellen einer Instanz der GolfApp
-golf_app = GolfApp()
-
-# Laden von Accounts und Golfplätzen
-golf_app.load_accounts()
-golf_app.load_courses()
-
-# Anzeige der geladenen Daten in Streamlit
-st.write("Muster-Accounts:")
-for account in golf_app.accounts:
-    st.header(f"**Benutzername:** {account['Benutzername']}, **E-Mail:** {account['E-Mail']}, **Mitgliedschaft:** {account['Mitgliedschaft']}")
-
-st.write("Golfplätze in der Schweiz:")
-for course in golf_app.courses:
-    st.write(f"**Name:** {course['Name']}, **Ort:** {course['Ort']}")
-    st.write(f"**Beschreibung:** {course['Beschreibung']}")
-    st.markdown("---")
--- Golf_App.py
+++ Golf_App.py
@@ -1,162 +1,42 @@
 import streamlit as st
 
-class Hole:
-    def __init__(self, hole_number, par, stroke_index):
-        self.hole_number = hole_number
-        self.par = par
-        self.stroke_index = stroke_index
+class GolfApp:
+    def __init__(self):
+        self.accounts = []
+        self.courses = []
 
-    def __str__(self):
-        return f"Hole {self.hole_number} - Par: {self.par}, Stroke Index: {self.stroke_index}"
+    def load_accounts(self):
+        # Muster-Accounts
+        self.accounts = [
+            {"Benutzername": "casual_golfer123", "E-Mail": "casualgolfer123@example.com", "Passwort": "Golfer123!", "Mitgliedschaft": "Basic"},
+            {"Benutzername": "golf_enthusiast45", "E-Mail": "golfenthusiast45@example.com", "Passwort": "GreenSwing45@", "Mitgliedschaft": "Premium"},
+            {"Benutzername": "pro_golfer89", "E-Mail": "progolfer89@example.com", "Passwort": "ProPassion89$", "Mitgliedschaft": "Elite"}
+        ]
+        st.write("Accounts wurden geladen!")
 
-class Player:
-    def __init__(self, name, handicap):
-        self.name = name
-        self.handicap = handicap
-        self.scores = {}
+    def load_courses(self):
+        # Golfplätze in der Schweiz
+        self.courses = [
+            {"Name": "Swiss Alps Golf Club", "Ort": "Crans-Montana", "Beschreibung": "Ein atemberaubender Golfplatz in den Schweizer Alpen mit Panoramablicken und anspruchsvollen Löchern."},
+            {"Name": "Lake Geneva Golf Resort", "Ort": "Genf", "Beschreibung": "Ein exklusiver Golfplatz am Genfer See mit Blick auf das Wasser und eine perfekte Kombination aus Herausforderung und Schönheit."},
+            {"Name": "Zurich Valley Golf Club", "Ort": "Zürich", "Beschreibung": "Ein moderner Golfplatz in der Nähe von Zürich, der eine erstklassige Spielumgebung mit hochwertigen Einrichtungen bietet."}
+        ]
+        st.write("Golfplätze wurden geladen!")
 
-    def play_hole(self, hole, strokes):p
-        self.scores[hole.hole_number] = strokes
+# Erstellen einer Instanz der GolfApp
+golf_app = GolfApp()
 
-    def calculate_total_score(self):
-        return sum(self.scores.values())
+# Laden von Accounts und Golfplätzen
+golf_app.load_accounts()
+golf_app.load_courses()
 
-    def __str__(self):
-        return f"{self.name} - Handicap: {self.handicap}, Total Score: {self.calculate_total_score()}"
+# Anzeige der geladenen Daten in Streamlit
+st.write("### Geladene Muster-Accounts:")
+for account in golf_app.accounts:
+    st.write(f"**Benutzername:** {account['Benutzername']}, **E-Mail:** {account['E-Mail']}, **Mitgliedschaft:** {account['Mitgliedschaft']}")
 
-class Course:
-    def __init__(self, name, holes):
-        self.name = name
-        self.holes = holes
-
-def get_player_info():
-    name = st.text_input("Enter player's name:")
-    handicap = st.number_input("Enter your handicap:", min_value=0)
-    return Player(name, handicap)
-
-def create_custom_course():
-    course_name = st.text_input("Enter the name of the course:")
-    holes = []
-    for i in range(1, 19):
-        st.write(f"Enter details for Hole {i}:")
-        par = st.number_input(f"Par for Hole {i}:", min_value=1)
-        stroke_index = st.number_input(f"Stroke Index for Hole {i}:", min_value=1)
-        holes.append(Hole(i, par, stroke_index))
-    return Course(course_name, holes)
-
-def get_course_choice():
-    st.write("Choose a golf course:")
-    st.write("1. Ostschweizerischer Golfclub Niederbüren")
-    st.write("2. Golfclub Appenzell")
-    st.write("3. Golfpark Waldkirch")
-    st.write("4. Create a new course")
-    choice = st.selectbox("Enter your choice (1-4):", ('1', '2', '3', '4'))
-    
-    if choice == '1':
-        predefined_holes = [Hole(1, 5, 13), Hole(2, 4, 9), ...]  # Define holes here
-        return Course("Ostschweizerischer Golfclub Niederbüren", predefined_holes)
-    # Rest of the code remains the same for other choices
-
-def main():
-    st.title("Golf Score Tracker")
-    course = get_course_choice()
-    players = []
-
-    while True:
-        player = get_player_info()
-        players.append(player)
-
-        for hole in course.holes:
-            st.write(hole)
-            strokes = st.number_input(f"Enter strokes for Hole {hole.hole_number}:")
-            player.play_hole(hole, strokes)
-
-        another_player = st.radio("Is there another player?", ('Yes', 'No'))
-        if another_player == 'No':
-            break
-
-    players.sort(key=lambda x: x.calculate_total_score())
-    st.header(f"Results at {course.name}:")
-    for player in players:
-        st.write(player)
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-from golf_app import current_user  # Stelle sicher, dass der Import korrekt ist
-
-if hasattr(golf_app, 'current_user') and golf_app.current_user:
-    st.sidebar.subheader(f"Logged in as: {golf_app.current_user.username}")
-
-    if golf_app.current_user.is_admin:
-        st.sidebar.info("Admin actions:")
-        admin_choice = st.sidebar.radio("Select an action", ["Add Golf Course", "Delete Golf Course", "Logout"])
-
-        if admin_choice == "Add Golf Course":
-            st.subheader("Add Golf Course:")
-            course_name = st.text_input("Enter the name of the course:")
-            holes = []
-            for i in range(1, 19):
-                st.write(f"Enter details for Hole {i}:")
-                par = st.number_input(f"Par for Hole {i}:", min_value=1, step=1)
-                stroke_index = st.number_input(f"Stroke Index for Hole {i}:", min_value=1, step=1)
-                holes.append({"hole_number": i, "par": par, "stroke_index": stroke_index})
-
-            if st.button("Add Golf Course"):
-                golf_app.courses.append(golf_app.create_custom_course(course_name, holes))
-                golf_app.save_courses()
-
-        elif admin_choice == "Delete Golf Course":
-            st.subheader("Delete Golf Course:")
-            if not golf_app.courses:
-                st.warning("No courses available for deletion.")
-            else:
-                course_options = [course.name for course in golf_app.courses]
-                course_to_delete = st.selectbox("Select a course to delete:", course_options)
-                if st.button("Delete Course"):
-                    golf_app.courses = [course for course in golf_app.courses if course.name != course_to_delete]
-                    golf_app.save_courses()
-                    st.success(f"Golf Course '{course_to_delete}' deleted.")
-
-        elif admin_choice == "Logout":
-            golf_app.current_user = None
-            st.success("Logged out.")
-            st.sidebar.warning("Admin logged out.")
-
-    else:  # User actions
-        st.sidebar.info("User actions:")
-        user_choice = st.sidebar.radio("Select an action", ["Let's play!", "Show previous games", "Compete with your puttpals", "Logout"])
-
-        if user_choice == "Let's play!":
-            st.subheader("Let's play!")
-            if not golf_app.courses:
-                st.warning("No courses available. Please add a course first.")
-            else:
-                course_options = [course.name for course in golf_app.courses]
-                selected_course = st.selectbox("Select a course to play:", course_options)
-
-                if st.button("Play"):
-                    course = next(course for course in golf_app.courses if course.name == selected_course)
-                    golf_app.current_user.player.play_round(course)
-                    golf_app.save_accounts()
-
-        elif user_choice == "Show previous games":
-            st.subheader("Show previous games:")
-            golf_app.current_user.player.show_previous_rounds()
-
-        elif user_choice == "Compete with your puttpals":
-            st.subheader("Compete with your puttpals:")
-            friend_username = st.text_input("Enter the username of your friend:")
-            friend = next((account for account in golf_app.accounts if account.username == friend_username), None)
-
-            if st.button("Compete"):
-                if friend and friend.is_admin:
-                    st.error("Cannot compete with an admin.")
-                elif friend:
-                    golf_app.current_user.player.compete_with_friend(friend.player)
-                else:
-                    st.warning(f"No user found with the username '{friend_username}'.")
-                    st.warning("Please enter a valid username.")
+st.write("### Geladene Golfplätze in der Schweiz:")
+for course in golf_app.courses:
+    st.write(f"**Name:** {course['Name']}, **Ort:** {course['Ort']}")
+    st.write(f"**Beschreibung:** {course['Beschreibung']}")
+    st.write("---")
