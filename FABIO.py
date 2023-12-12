605d98c04db4250a420101573093abd46c4b4313
anaantunes26 <152395214+anaantunes26@users.noreply.github.com>
2023-12-12 01:09:45 +0100

    ya

--- FABIO.py
+++ FABIO.py
@@ -43,3 +43,20 @@ elif menu_choice == "Exit":
     st.balloons()
     st.write("Goodbye!")
     st.stop()
+
+if golf_app.current_user:
+    st.sidebar.subheader(f"Logged in as: {golf_app.current_user.username}")
+
+    if golf_app.current_user.is_admin:
+        st.sidebar.info("Admin actions:")
+        admin_choice = st.sidebar.radio("Select an action", ["Add Golf Course", "Delete Golf Course", "Logout"])
+
+        if admin_choice == "Add Golf Course":
+            st.subheader("Add Golf Course:")
+            course_name = st.text_input("Enter the name of the course:")
+            holes = []
+            for i in range(1, 19):
+                st.write(f"Enter details for Hole {i}:")
+                par = st.number_input(f"Par for Hole {i}:", min_value=1, step=1)
+                stroke_index = st.number_input(f"Stroke Index for Hole {i}:", min_value=1, step=1)
+                holes.append({"hole_number": i, "par": par, "stroke_index": stroke_index})
