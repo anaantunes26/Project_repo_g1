6aea28889a4efce305f5a77b0a2ec6f11857eaeb
anaantunes26 <152395214+anaantunes26@users.noreply.github.com>
2023-12-12 01:01:06 +0100

    n

--- Golf_App.py
+++ Golf_App.py
@@ -1,34 +1,30 @@
 class GolfApp:
     def __init__(self):
         self.accounts = []
-        self.courses = []
 
     def load_accounts(self):
-        # Muster-Accounts
-        self.accounts = [
-            {"Benutzername": "casual_golfer123", "E-Mail": "casualgolfer123@example.com", "Passwort": "Golfer123!", "Mitgliedschaft": "Basic"},
-            {"Benutzername": "golf_enthusiast45", "E-Mail": "golfenthusiast45@example.com", "Passwort": "GreenSwing45@", "Mitgliedschaft": "Premium"},
-            {"Benutzername": "pro_golfer89", "E-Mail": "progolfer89@example.com", "Passwort": "ProPassion89$", "Mitgliedschaft": "Elite"}
-        ]
+        # Hier könntest du vorhandene Accounts laden, wenn nötig
+        pass
 
-    def load_courses(self):
-        # Golfplätze in der Schweiz
-        self.courses = [
-            {"Name": "Swiss Alps Golf Club", "Ort": "Crans-Montana", "Beschreibung": "Ein atemberaubender Golfplatz in den Schweizer Alpen mit Panoramablicken und anspruchsvollen Löchern."},
-            {"Name": "Lake Geneva Golf Resort", "Ort": "Genf", "Beschreibung": "Ein exklusiver Golfplatz am Genfer See mit Blick auf das Wasser und eine perfekte Kombination aus Herausforderung und Schönheit."},
-            {"Name": "Zurich Valley Golf Club", "Ort": "Zürich", "Beschreibung": "Ein moderner Golfplatz in der Nähe von Zürich, der eine erstklassige Spielumgebung mit hochwertigen Einrichtungen bietet."}
-        ]
-
-    def get_accounts(self):
-        return self.accounts
-
-    def get_courses(self):
-        return self.courses
+    def create_account(self, first_name, last_name, handicap, username, password, is_admin):
+        new_account = {
+            "First Name": first_name,
+            "Last Name": last_name,
+            "Handicap": handicap,
+            "Username": username,
+            "Password": password,
+            "Is Admin": is_admin
+        }
+        self.accounts.append(new_account)
+        return new_account
 
 # Beispiel, wie die GolfApp verwendet werden könnte:
 # golf_app = GolfApp()
 # golf_app.load_accounts()
-# golf_app.load_courses()
-# accounts = golf_app.get_accounts()
-# courses = golf_app.get_courses()
-# ... (weitere Verarbeitung der geladenen Daten)
+
+# Hier würdest du vermutlich Benutzereingaben erhalten und dann create_account() aufrufen
+# username = "neuer_benutzername"
+# password = "passwort123"
+# usw.
+# golf_app.create_account("Vorname", "Nachname", 18, username, password, False)
+# ... (weitere Verarbeitung der Accounts)
